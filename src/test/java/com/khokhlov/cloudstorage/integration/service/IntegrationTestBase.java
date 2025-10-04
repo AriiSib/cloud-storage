ae01892ac5a6975bde6b143a22f4bad638031161
Artemii_Khokhlov <artemii.hhlov@gmail.com>
2025-10-04 23:27:11 +0600

    test: introduce IntegrationTestBase with PostgreSQL 16 container

---
+++ src/test/java/com/khokhlov/cloudstorage/integration/service/IntegrationTestBase.java
@@ -0,0 +1,23 @@
+package com.khokhlov.cloudstorage.integration.service;
+
+import com.khokhlov.cloudstorage.integration.annotation.IT;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@IT
+@Testcontainers
+public abstract class IntegrationTestBase {
+
+    @Container
+    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    @DynamicPropertySource
+    static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
+}
