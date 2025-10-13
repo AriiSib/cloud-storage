d9ffa8fa392bee41c525a015370de43d9540cc83
Artemii_Khokhlov <artemii.hhlov@gmail.com>
2025-10-13 13:37:01 +0600

    feat(storage): add Minio config and auto-create bucket on startup

---
+++ src/main/java/com/khokhlov/cloudstorage/config/MinioConfig.java
@@ -0,0 +1,44 @@
+package com.khokhlov.cloudstorage.config;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import lombok.Data;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "minio")
+public class MinioConfig {
+    private String endpoint;
+    private String accessKey;
+    private String secretKey;
+    private String bucketName;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+    @Bean
+    public CommandLineRunner ensureBucket(MinioClient minio) {
+        return args -> {
+            boolean exists = minio.bucketExists(
+                    BucketExistsArgs.builder()
+                            .bucket(bucketName)
+                            .build());
+            if (!exists) {
+                minio.makeBucket(
+                        MakeBucketArgs.builder()
+                                .bucket(bucketName)
+                                .build());
+            }
+        };
+    }
+}
