85490eea429260fe4deb083b2958a87325c797b8
Artemii_Khokhlov <artemii.hhlov@gmail.com>
2025-11-18 20:46:56 +0600

    refactor: refactoring SecurityConfig

--- src/main/java/com/khokhlov/cloudstorage/config/security/SecurityConfig.java
+++ src/main/java/com/khokhlov/cloudstorage/config/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -16,11 +15,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.session.*;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
-import org.springframework.security.web.session.SimpleRedirectInvalidSessionStrategy;
 
 @Configuration
 @EnableWebSecurity
@@ -33,7 +30,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filter(HttpSecurity http) throws Exception {
         return http
-                .securityMatcher("/api/**")
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(restAuthenticationEntryPoint))
@@ -46,10 +42,8 @@ public class SecurityConfig {
 
     @Profile("dev")
     @Bean
-    @Order(1)
     public SecurityFilterChain apiFilter(HttpSecurity http) throws Exception {
         return http
-                .securityMatcher("/api/**")
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
@@ -57,51 +51,13 @@ public class SecurityConfig {
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint(restAuthenticationEntryPoint)
                 )
-//                .anonymous(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/", "/login", "/registration", "/files/**",
                                 "/index.html", "/config.js", "/assets/**",
-                                "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**"
-                        ).permitAll()
+                                "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
                         .requestMatchers("/api/auth/sign-up", "/api/auth/sign-in").permitAll()
                         .anyRequest().authenticated()
-                )
-                .build();
-    }
-
-    @Profile("dev")
-    @Bean
-    @Order(2)
-    public SecurityFilterChain webFilter(HttpSecurity http) throws Exception {
-        return http
-                .securityMatcher("/**")
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(sm -> sm
-                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                        .invalidSessionUrl("/login")
-                        .invalidSessionStrategy(new SimpleRedirectInvalidSessionStrategy("/login"))
-                )
-                .exceptionHandling(ex -> ex
-                        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
-                )
-
-//                .anonymous(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/", "/login", "/registration", "/files/**",
-                                "/index.html", "/config.js", "/assets/**",
-                                "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**"
-                        ).permitAll()
-                        .requestMatchers("/api/auth/sign-up", "/api/auth/sign-in").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(AbstractHttpConfigurer::disable)
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .clearAuthentication(true)
-                        .invalidateHttpSession(true)
-                        .deleteCookies("SESSION")
                 )
                 .build();
     }
