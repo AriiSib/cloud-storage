52ff0aee715c45f7db464e1913fcb51049aadf03
Artemii_Khokhlov <artemii.hhlov@gmail.com>
2025-11-19 22:12:10 +0600

    fix: fix username length for AuthRequest

--- src/main/java/com/khokhlov/cloudstorage/model/dto/request/AuthRequest.java
+++ src/main/java/com/khokhlov/cloudstorage/model/dto/request/AuthRequest.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size;
 @Schema(description = "Request with username and password")
 public record AuthRequest(
         @Schema(example = "JohnDoe")
-        @NotBlank @Size(min = 4, max = 20) String username,
+        @NotBlank @Size(min = 5, max = 20) String username,
 
         @Schema(example = "123_Ab.()+-{}@")
         @NotBlank @Size(min = 6, max = 32) String password
