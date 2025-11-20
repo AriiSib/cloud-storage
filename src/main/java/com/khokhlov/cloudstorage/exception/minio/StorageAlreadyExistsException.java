8ca281b9e03092c57c48e4ca6657cafe2de3d6f2
Artemii_Khokhlov <artemii.hhlov@gmail.com>
2025-11-20 20:02:52 +0600

    refactor: wrap minio exceptions in custom ones

--- src/main/java/com/khokhlov/cloudstorage/exception/minio/StorageAlreadyExistsException.java
+++ src/main/java/com/khokhlov/cloudstorage/exception/minio/StorageAlreadyExistsException.java
@@ -1,7 +1,17 @@
 package com.khokhlov.cloudstorage.exception.minio;
 
 public class StorageAlreadyExistsException extends StorageException {
-    public StorageAlreadyExistsException(String filename) {
-        super("Resource already exists: " + filename);
+
+    public StorageAlreadyExistsException(String resource) {
+        super(messageFor(resource));
     }
+
+    public StorageAlreadyExistsException(String resource, Throwable cause) {
+        super(messageFor(resource), cause);
+    }
+
+    private static String messageFor(String resource) {
+        return "Resource already exists: " + resource;
+    }
+
 }
--- src/main/java/com/khokhlov/cloudstorage/exception/minio/StorageNotFoundException.java
+++ src/main/java/com/khokhlov/cloudstorage/exception/minio/StorageNotFoundException.java
@@ -4,4 +4,9 @@ public class StorageNotFoundException extends StorageException {
     public StorageNotFoundException(String message) {
         super(message);
     }
+
+    public StorageNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
